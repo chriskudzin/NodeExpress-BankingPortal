893cdaa2991b1a15ca18ab3cfb2cffd9d8e1b390
Chris Kudzin <cpkudzin@gmail.com>
2019-01-29 17:08:20 +0100

    Updated app.js.

--- src/app.js
+++ src/app.js
@@ -30,6 +30,10 @@ app.get('/credit', (req, res) => {
   res.render('account', {account: accounts.credit });
 });
 app.get('/transfer', (req, res) => res.render('transfer'));
+app.post('/transfer', (req, res) => {
+  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
+});
 
 app.get('/profile', (req, res) =>  { res.render('profile', { user: users[0] }); })
 app.listen(3000, () => console.log('PS Project Running on port 3000'));
